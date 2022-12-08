d9758051f7a5d78d63ca38a112ceb1ec66c1cf79
najahyuga <najahyugad@gmail.com>
2022-12-08 08:12:28 +0700

    Update code MainActivity

--- app/src/main/java/com/example/latihanrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/latihanrecyclerview/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.latihanrecyclerview;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -39,19 +41,25 @@ public class MainActivity extends AppCompatActivity {
 
     public class BarangAdapter extends RecyclerView.Adapter<BarangHolder>{
         ArrayList<Barang> all_barang;
+        private LayoutInflater inflater;
         public BarangAdapter(Context ctx, ArrayList<Barang> all_barang) {
-        this.all_barang =  all_barang;
+            this.all_barang =  all_barang;
+            inflater = LayoutInflater.from(ctx);
         }
 
         @NonNull
         @Override
         public BarangHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            View item = inflater.inflate(R.layout.listbarang, parent, false);
+            return new BarangHolder(item, this);
         }
 
         @Override
         public void onBindViewHolder(@NonNull BarangHolder holder, int position) {
-
+            Barang perBarang = this.all_barang.get(position);
+            holder.namaBarang.setText(perBarang.nama_barang);
+            holder.hargaBarang.setText(String.valueOf(perBarang.harga_barang));
+            holder.stokBarang.setText(String.valueOf(perBarang.stok_barang));
         }
 
         @Override
@@ -64,9 +72,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.all_barang = new ArrayList<Barang>();
         all_barang.add(new Barang("Air", 2000, 10));
         all_barang.add(new Barang("Snack", 1000, 5));
         recyclerView = findViewById(R.id.recyclerView);
+        BarangAdapter adapter = new BarangAdapter(this, this.all_barang);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
 }
